52f5e4ec1550eb5703a94c57b7f88441c83f3bcc
TomateDeveloper <ianfe@martina.com.co>
2019-10-11 13:54:59 -0500

    Updated staff component

--- src/app/components/application/staff/application.staff.component.ts
+++ src/app/components/application/staff/application.staff.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
 
+import {faDiscord, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'application-staff',
   templateUrl: './application.staff.component.html'
 })
 
 export class ApplicationStaffComponent {
+
+  faTwitter = faTwitter;
+  faDiscord = faDiscord;
+  faEnvelope = faEnvelope;
+
+
 }
